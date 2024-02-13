a4354e20e6e6f6dd8e875447398850bf05e50272
Endo Axelsson <endo.axelsson@elev.ga.ntig.se>
2024-02-13 11:16:18 +0100

    ⚡️ databas

--- readme.md
+++ readme.md
@@ -4,3 +4,7 @@ Ska göra en databas för olika maträtters rescensioner.
 https://app.diagrams.net/#G11NYRwmT81ZiikNbQCNkv0FfkIpOMcT0x
 
 ![alt](drawiomat.JPG)
+
+2024-02-06
+Klargjorde layout. En sidebar med lite av restaurangens information och loggan.
+På andra sidan ligger recensioner om maten.
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,27 @@
 const express = require('express')
 const router = express.Router()
+const pool = require('../db')
 
 router.get('/', function (req, res) {
   res.render('index.njk', { title: 'Welcome' })
+
 })
 
+  router.get('/db/:id', async (req, res) => {
+    try {
+      const id = req.params.id
+  
+      const [options] = await pool
+        .promise()
+        .query(`SELECT * FROM endo_dish_score WHERE id = ${id}`)
+      res.json({
+      
+      })
+    } catch (error) {
+      console.log(error)
+      res.sendStatus(500)
+
+    }
+
+})
 module.exports = router
