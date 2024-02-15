8e631c7849282d3dbc75bd68b35edeecc730e7da
Endo Axelsson <endo.axelsson@elev.ga.ntig.se>
2024-02-15 11:16:41 +0100

    ✨ Review sida

--- routes/index.js
+++ routes/index.js
@@ -6,10 +6,32 @@ router.get('/', function (req, res) {
   res.render('index.njk', { title: 'Welcome' })
 
 })
+router.get('/review', async function (req, res) {
+  try {
+    const id = req.params.id 
+    
+    return res.render('review.njk', {
+    })
+  } catch (error) {
+    console.log(error)
+    res.sendStatus(500)
+  }
+})
+
+router.get('/review/:id', async (req, res) => {
+  try {
+    const id = req.params.id
+  }  catch (error) {
+    console.log(error)
+    res.sendStatus(500)
+  }
+  })
 
   router.get('/db/:id', async (req, res) => {
     try {
       const id = req.params.id
+
+      
   
       const [options] = await pool
         .promise()
@@ -22,6 +44,6 @@ router.get('/', function (req, res) {
       res.sendStatus(500)
 
     }
-
+  
 })
 module.exports = router
